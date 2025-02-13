8f21a3f8bf4e49dcfebe5dc834d4dd14bfa1b875
ChristmasLover12345 <madnes0205@gmail.com>
2025-02-13 14:04:14 -0800

    added dropdown menu to the front end. starting to work on sorting logic now

--- Scripts/app.js
+++ Scripts/app.js
@@ -2,29 +2,49 @@ import { GetJSON } from "./fetch.js";
 
 const contentDisplay = document.getElementById("contentDisplay");
 
+const nextBtn = document.getElementById("nextBtn");
+const previousBtn = document.getElementById("previousBtn");
+
+const pages10Btn = document.getElementById("pages10Btn");
+const pages20Btn = document.getElementById("pages20Btn");
+const pages30Btn = document.getElementById("pages30Btn");
+const pages40Btn = document.getElementById("pages40Btn");
+const pages50Btn = document.getElementById("pages50Btn");
+
+const sortIDBtn = document.getElementById("sortIDBtn");
+const sortFirstNameBtn = document.getElementById("sortFirstNameBtn");
+const sortLastNameBtn = document.getElementById("sortLastNameBtn");
+const sortHeightBtn = document.getElementById("sortHeightBtn");
+const sortAgeBtn = document.getElementById("sortAgeBtn");
+
+let peopleArray = [];
 let pageSize = 10;
 let currentPage = 1;
+let totalPages = 1;
 
 
-
+// Get data and set up the people array + total pages
 const setStuff = async () => {
     const whatever = await GetJSON();
-    let poepleArray = whatever.People
-    console.log(poepleArray);
-    return poepleArray;
+    peopleArray = whatever.People;
+    totalPages = Math.ceil(peopleArray.length / pageSize);
+    console.log(peopleArray);
+    return peopleArray;
 }
 
 
-
+// Get items for the current page
 async function getItems(page) 
 {
-    const test = await setStuff();
+
     const start = (page - 1) * pageSize;
-    return test.slice(start, start + pageSize);
+    return peopleArray.slice(start, start + pageSize);
 }
 
+// Display the current page
 async function displayCurrentPage()
 {
+    contentDisplay.innerHTML = "";
     const items = await getItems(currentPage);
     for (let item of items) 
     {
@@ -52,23 +72,40 @@ async function displayCurrentPage()
     }
 }
 
-function nextPage()
+// Pagination functions
+async function nextPage()
 {
     if (currentPage < totalPages) 
     {
         currentPage++;
-        displayCurrentPage();
+        await displayCurrentPage();
     }
 }
 
-function previousPage() 
+// Pagination functions
+async function previousPage() 
 {
     if (currentPage > 1)
     {
         currentPage--;
-        displayCurrentPage();
+        await displayCurrentPage();
     }
 }
 
-displayCurrentPage();
-    
+
+nextBtn.addEventListener('click', () => {
+    nextPage();
+})
+
+previousBtn.addEventListener('click', () => {
+    previousPage();
+})
+
+// Onload function
+async function initialize() {
+   
+    await setStuff();
+    displayCurrentPage();
+}
+initialize();
+
